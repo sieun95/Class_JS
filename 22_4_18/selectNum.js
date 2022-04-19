232ea1da03c46c34b5a791ab98d294701f55bcb7
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-04-19 10:25:54 +0900

    lse
    lse

--- 22_4_18/selectNum.js
+++ 22_4_18/selectNum.js
@@ -1,11 +1,21 @@
+// 이시은
+// const selectNum = prompt("숫자를 입력하세요");
 
-const selectNum = prompt("숫자를 입력하세요", );
+// let randomNum = Math.random() * 7 + 3; //3.0 ~ 10.0 사이의 랜덤한 값(난수)
+// randomNum = Math.floor(randomNum);
 
-let randomNum = Math.random() * 7 + 3;    //3.0 ~ 10.0 사이의 랜덤한 값(난수)
-randomNum = Math.floor(randomNum); 
+// if (selectNum == randomNum) {
+//   alert("정답");
+// } else {
+//   alert("오답");
+// }
 
-if(selectNum == randomNum) {
-     console.log("정답")
-}else {
-    console.log("오답")
+// 교수님
+let inputNum = prompt("숫자를 입력하세요");
+
+let correctNum = Math.floor((Math.random() * 10)) % 8 + 3;    //3 + 0 ~ 3 + 7
+if(inputNum == correctNum){
+    alert("정답!!")
+}else{
+    alert("오답!")
 }
